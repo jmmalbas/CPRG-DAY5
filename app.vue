59f8f1c177addc10708549a3a1bf49a54ae9d565
John Michael Malbas <923415@ACDM.DS.SAIT.CA>
2024-01-19 13:04:38 -0700

    Initial files

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <AppHeader />
+
+  <div class="main">
+    <NuxtPage />
+  </div>
+  <AppFooter />
+</template>
+<style>
+body {
+  margin: 0;
+}
+.main {
+  height: 58vh;
+}
+</style>
